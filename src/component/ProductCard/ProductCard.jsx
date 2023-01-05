7170a879e8876ff63acf82c8baabf4497946eb21
Abjerry97 <abj97x@gmail.com>
2023-01-05 06:43:07 -0700

    update commit

--- src/component/ProductCard/ProductCard.jsx
+++ src/component/ProductCard/ProductCard.jsx
@@ -17,8 +17,8 @@ function ProductCard(props) {
           <div className={` ${productCategoryColor[product?.category] || "bg-gray"}  d-flex justify-content-center align-items-center`}>
               <img src={product?.image} alt="imani-bahati" />
             </div>
-            <div className="p-3 footer row">
-                <div className="col-7 product-card-text ">
+            <div className="pt-3 px-2 px-md-3 footer row m-0">
+                <div className="col-7 product-card-text p-0">
                     <h5 className="m-0"> {product?.name}</h5>
                     <p className="m-0">{product?.newPrice}</p>
                     <p className="m-0">{product?.price}</p>
