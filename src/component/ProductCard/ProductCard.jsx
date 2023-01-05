6075cfc3346f423b9a8da79af98d6fdb15323cc8
Abjerry97 <abj97x@gmail.com>
2023-01-05 06:36:40 -0700

    update commit

--- src/component/ProductCard/ProductCard.jsx
+++ src/component/ProductCard/ProductCard.jsx
@@ -14,16 +14,17 @@ function ProductCard(props) {
   return (  
     <Col>
           <div className=" shadow  product-card position-relative">
-          <div className={`${productCategoryColor[product?.category] || "bg-gray"}  d-flex justify-content-center align-items-center`}>
+          <div className={` ${productCategoryColor[product?.category] || "bg-gray"}  d-flex justify-content-center align-items-center`}>
               <img src={product?.image} alt="imani-bahati" />
             </div>
-            <div className="p-3 footer">
-                <div className="product-card-text">
+            <div className="p-3 footer row">
+                <div className="col-7 product-card-text ">
                     <h5 className="m-0"> {product?.name}</h5>
                     <p className="m-0">{product?.newPrice}</p>
                     <p className="m-0">{product?.price}</p>
                 </div>
-                <div className="product-card-btn">Add to cart</div>
+                <div className=" col-5 p-0">
+                  <div className="product-card-btn">Add to cart</div>  </div>
             </div>
           </div>
         </Col>
